f125cf3b87ea9805e876c65cdd1e62812898ae5f
Mostafa Anwar <39226625+MostafAanwar@users.noreply.github.com>
2018-09-20 04:53:52 +0200

    Add files via upload

---
+++ WhaleTalk.js
@@ -0,0 +1,20 @@
+/*Take a phrase like 'turpentine and turtles' and translate it into its "whale talk" equivalent: 'UUEEIEEAUUEE'.*/
+/*There are a few simple rules for translating text to whale language:*/
+/*1 There are no consonants. Only vowels excluding "y".*/
+/*2 The u's and e's are extra long, so we must double them in our program.*/
+let input='the cake is a lie';
+const vowels=['a', 'e', 'i', 'o', 'u'];
+let resultArray=[];
+for(let i=0;i<input.length;i++)
+  {
+    for(let j=0;j<vowels.length;j++)
+    {
+    	if(input[i]==vowels[j])  
+       resultArray.push(input[i]);
+    }
+    	if(input[i]=='e')  
+       		resultArray.push(input[i]);
+    if(input[i]=='u')  
+       		resultArray.push(input[i]);
+  }
+console.log(resultArray.join('').toUpperCase());
